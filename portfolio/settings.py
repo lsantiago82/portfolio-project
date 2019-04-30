c782aa5a644f5240136bb4b3023e8f8ea0e15c30
rnominato <rnominato@gmail.com>
2019-04-29 23:30:29 -0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '880029',
+        'PASSWORD': 'XXXXXX',
         'HOST': 'localhost',
         'PORT': '5432',
 
